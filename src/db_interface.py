f376b5a62d02de2ef08eb09bf2dcb66ae44a1148
Tejas <tejaskothari03@gmail.com>
2023-07-26 04:59:50 +0530

    introduced default values, get tables, copy to db

--- src/db_interface.py
+++ src/db_interface.py
@@ -1,16 +1,19 @@
 import vertica_python
+
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = '5433'
+DEFAULT_USER = 'dbadmin'
+DEFAULT_PWD = 'no_default_enter_pwd'
 class DBInterface:
-    """
-    Database Interface for DB operations
-    """
     def __init__(self, config):
         # Private attribute to store connection info
         self._conn_info = {
-            'host': config['host'],
-            'port': config['port'],
-            'database': config['database'],
-            'user': config['user'],
-            'password': config['password']
+            'host': config('host', DEFAULT_HOST),
+            'port': config('port', DEFAULT_PORT),
+            # CHANGED FOR A SPECIFIC USECASE
+            'database': config.get('vertica-database', DEFAULT_DATABASE),
+            'user': config.get('user', DEFAULT_USER),
+            'password': config('password', DEFAULT_PWD)
         }
 
     def __enter__(self):
@@ -23,3 +26,23 @@ class DBInterface:
         # Close the cursor and connection when the context exits
         self._cursor.close()
         self._connection.close()
+
+    def copy_csv_to_table(self, csv_file_path, table_name, schema):
+        # Use the COPY command to copy data from the CSV file into the Vertica table 
+        # This is applicable when the file has no value requiring conversions
+        with open(csv_file_path, 'rb') as csv_file:
+            with self as cursor:
+                cursor.copy(f"COPY {schema}.{table_name} FROM STDIN DELIMITER ','", csv_file, buffer_size=65536)
+
+    def get_all_tables(self, schema):
+        # Retrieve a list of all table names in the schema
+        with self as cursor:
+            cursor.execute(f"SELECT table_name FROM v_catalog.tables WHERE table_schema = '{schema}'")
+            return [row[0] for row in cursor.fetchall()]
+
+    def copy_table_data_to_csv(self, dest_db_interface, table_name, dest_file_path, schema):
+        # Use the COPY command to copy data from the source table to the CSV file
+        with dest_db_interface as cursor:
+            copy_command = f"COPY {schema}.{table_name} TO STDOUT DELIMITER ','"
+            with open(dest_file_path, 'w') as dest_file:
+                cursor.copy(copy_command, dest_file)
