c832a63af24a6ac7ed57ebc4f7f31a48b3d903ba
Tejas <tejaskothari03@gmail.com>
2023-07-27 01:12:36 +0530

    added v6 proper query

--- src/libs/column_config.py
+++ src/libs/column_config.py
@@ -8,7 +8,8 @@ which tables or schema they belong to.
 TIME_BASED = set()
 # SELECT INET_ATON(HEX_TO_INTEGER(TO_HEX('varchar/string'))) FOR IPv4 32bit address;
 IPV4_BASED = set()
-# SELECT V6_NTOA('varchar/string'))) FOR IPv6 64bit address;
+# SELECT V6_NTOA('varbinary'))) FOR IPv6 64bit address from varbinary to varchar
+# SELECT V6_ATON('varchar/string'))) FOR IPv6 64bit address from varchar to varbinary
 IPV6_BASED = set()
 NUMERICAL_BASED = set()
 
