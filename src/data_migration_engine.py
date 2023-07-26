9ebca6f5f74d8cc98a9d38b243f2b1575265cc96
Tejas <tejaskothari03@gmail.com>
2023-07-26 06:52:33 +0530

    cols fetching from csv method

--- src/data_migration_engine.py
+++ src/data_migration_engine.py
@@ -4,7 +4,23 @@ class DataEngine:
     def convert_to_varbinary(value):
         # Convert the IP address to bytes in varbinary format
         return bytes(value, encoding='utf-8')
-    
+
+    @staticmethod
+    def get_csv_columns(csv_file_path):
+        # Read the first row of the CSV file to get column names or count columns
+        with open(csv_file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            try:
+                columns = next(reader)
+            except StopIteration:
+                # If the header row is missing, count the number of columns in the first row
+                print("Header is missing in this particular file, returning column count instead!")
+                csv_file.seek(0)  # Reset the file pointer to the beginning of the file
+                first_row = next(reader)
+                column_count = len(first_row)
+                return column_count
+        return columns
+
     @staticmethod
     def convert_to_varbinary(value):
         # Convert the IP address to bytes in varbinary format
