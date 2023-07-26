16838326b60fa1a756d8a0eefcbefc6d631c14fb
Tejas <tejaskothari03@gmail.com>
2023-07-27 01:10:00 +0530

    a point to consider about missing header in CSVs

--- src/data_migration_engine.py
+++ src/data_migration_engine.py
@@ -1,6 +1,8 @@
 from libs.column_config import *
+import socket
 
-
+# Note :- IF HEADER ISN'T THERE
+# SHOULD WE FETCH FROM DB & DO?
 class DataEngine:
 
     @staticmethod
@@ -40,7 +42,11 @@ class DataEngine:
             else db_columns == csv_columns
 
     @staticmethod
-    def column_conversion_index(db_columns, row):
+    def column_conversion_index(db_columns):
+        if isInstance(db_columns, int):
+            # Can't process, we got int value (count of columns)
+            # headers are missing in the CSV.
+            return []
         timestampz_column_indexs = set()
         ipv4_column_indexs = set()
         ipv6_column_indexs = set()
@@ -56,6 +62,13 @@ class DataEngine:
                 ipv6_column_indexs.add(i)
             elif db_columns[i] in NUMERICAL_BASED:
                 numerical_column_indexs.add(i)
+        column_indexes = {
+            'timestampz_column_indexs':timestampz_column_indexs,
+            'ipv4_column_indexs':ipv4_column_indexs,
+            'ipv6_column_indexs':ipv6_column_indexs,
+            'numerical_column_indexs':numerical_column_indexs
+        }
+        return column_indexes
 
     @staticmethod
     def convert_string_to_varbinary(value):
@@ -63,12 +76,21 @@ class DataEngine:
         return bytes(value, encoding='utf-8')
 
     @staticmethod
-    def convert_string_to_timestampz(value):
-        # Convert the value to a timestamp with time zone!
-        # Databases having different timezones can be ISSUE
-        # from datetime import datetime
-        # return datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
-        return value
+    def convert_string_to_timestampz(value, timezone_str='UTC'):
+        # Convert the value to a timestamp obj with UTC time zone!
+        # UTC is very much important as src, db might be configured
+        # to different timezones and this will sync the records.
+        dt = datetime.strptime(value, '%Y-%m-%d %H:%M:%S:%f')
+        try:
+            timezone = pytz.timezone(timezone_str)
+        except pytz.UnknownTimeZoneError:
+            print(f"Invalid time zone: {timezone_str}")
+            return None
+        # Assign the time zone to the datetime object
+        dt_with_tz = timezone.localize(dt)
+        # Convert the datetime object to a timestampz string
+        timestampz_str = dt_with_tz.strftime('%Y-%m-%d %H:%M:%S.%f %Z')
+        return timestampz_str
 
     @staticmethod
     def convert_string_to_numeric(value):
@@ -82,4 +104,12 @@ class DataEngine:
         # Convert the value to an integer type
         # If integer values are in string format:
         # return int(value)
-        return value
+        return 
+    
+    @staticmethod
+    def convert_string_to_ipv6_varbinary(ipv6_address):
+        try:
+            binary_address = socket.inet_pton(socket.AF_INET6, ipv6_address)
+            return binary_address
+        except socket.error:
+            raise ValueError(f"Invalid IPv6 address: {ipv6_address}")
