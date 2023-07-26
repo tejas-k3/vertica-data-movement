840aaa3d56fca3fbf0a8c2f2df1e8a2fdc937060
Tejas <tejaskothari03@gmail.com>
2023-07-26 22:47:24 +0530

    Column data types

--- src/libs/column_config.py
+++ src/libs/column_config.py
@@ -1,3 +1,15 @@
 """
 Column's type for value conversions.
+Here, add column names in their respective
+sets, these columns are independent of 
+which tables or schema they belong to.
 """
+# SELECT TO_TIMESTAMP('varchar/string')
+TIME_BASED = set()
+# SELECT INET_ATON(HEX_TO_INTEGER(TO_HEX('varchar/string'))) FOR IPv4 32bit address;
+IPV4_BASED = set()
+# SELECT V6_NTOA('varchar/string'))) FOR IPv6 64bit address;
+IPV6_BASED = set()
+NUMERICAL_BASED = set()
+
+__all__ = [TIME_BASED, IPV4_BASED, IPV6_BASED, NUMERICAL_BASED]
